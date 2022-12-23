e6d3248dff411f0a26f3dc246e3cb7aab4da9a4a
lcmsde <lcmsde@protonmail.com>
2022-12-23 14:40:25 +0100

    Init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,10 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
+    var a = 3
+    println(a)
+    a=4
+    val b: Int = 4
+    println(a+b)
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test
+
+internal class MainKtTest {
+
+    @Test
+    fun main() {
+    }
+}
